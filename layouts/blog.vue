b454d1950da726ab66ba95fa3c796c5be2d5d8d1
Sinyi <buns3366@gmail.com>
2022-11-01 16:41:39 +0800

    feat: 套用layout

---
+++ layouts/blog.vue
@@ -0,0 +1,7 @@
+<!-- 於pages/*.vue 中可使用layout: blog 套用此layout -->
+<template>
+  <div>
+    <div>套用layout</div>
+    <nuxt />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,6 @@
 <script>
 export default {
   name: 'IndexPage',
+  layout: 'blog' // 使用layouts/blog.vue的佈局
 }
 </script>
